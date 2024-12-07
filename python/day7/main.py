714283cb5124124b5052860c9cb0be0c22c8200b
Jove Zhou <jove.sh@hotmail.com>
2024-12-07 14:52:19 +0900

    day7

--- python/day7/main.py
+++ python/day7/main.py
@@ -1,19 +1,60 @@
 import re
-# filename = "input.txt"
-filename = "sample.txt"
+from itertools import product
+filename = "input.txt"
+# filename = "sample.txt"
+data = []
+valid_data = []
+
 with open(filename) as file:
-    grid = [list(line.strip()) for line in file.readlines()]
+    for line in file:
+        numbers = list(map(int, re.findall(r"\d+", line.strip())))
+        data.append(numbers)
+print(data)
 
 
-rows, cols = len(grid), len(grid[0])
+def check_valid(line_data, flag="part1"):
+    target = line_data[0]
+    check_combinations(line_data[1:], target)
+
+
+def check_combinations(line_data, target, flag="part1"):
+    # print(line_data)
+    if flag == "part2":
+        operators = list(product(['+', '*', '||'], repeat=len(line_data) - 1))
+    else:
+        operators = list(product(['+', '*'], repeat=len(line_data) - 1))
+    comb_result = [0] * len(operators)
+    for i in range(len(operators)):
+        # print(operators[i])
+        ops = operators[i]
+        comb_result[i] = line_data[0]
+        for j in range(len(ops)):
+            # print(line_data[j], ops[j], line_data[j + 1])
+            if ops[j] == '+':
+                comb_result[i] += line_data[j + 1]
+            elif ops[j] == '*':
+                comb_result[i] *= line_data[j + 1]
+            elif ops[j] == '||':
+                comb_result[i] = int(str(comb_result[i]) + str(line_data[j + 1]))
+        if comb_result[i] == target:
+            valid_data.append(target)
+            return
+    # print(comb_result)
 
 
 def part1():
-    return 0
+    # check_valid(data[1])
+    for line_data in data:
+        check_valid(line_data)
+    total = sum(valid_data)
+    return total
 
 
 def part2():
-    return 0
+    for line_data in data:
+        check_valid(line_data, "part2")
+    total = sum(valid_data)
+    return total
 
 
 print("Part one:", part1())
